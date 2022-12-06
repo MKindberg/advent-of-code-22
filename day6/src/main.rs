42ad55b318b1f64c3f42ce761a71ffa73e799874
Marcus <6180175+MKindberg@users.noreply.github.com>
2022-12-06 20:38:53 +0100

    Add day6 without hashset

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,10 +7,19 @@ fn message_start(data: &str, l: usize) -> usize {
         .unwrap()
         + l
 }
+fn message_start2(data: &str, l: usize) -> usize {
+    data.as_bytes()
+        .windows(l)
+        .position(|w| w.iter().map(|x| 1 << (x - b'a')).sum::<u32>().count_ones() as usize == l)
+        .unwrap()
+        + l
+}
 
 fn main() {
     let input = fs::read_to_string("input").expect("Failed to read input file");
 
     println!("Part 1: {}", message_start(&input, 4));
+    println!("Part 1: {}", message_start2(&input, 4));
     println!("Part 2: {}", message_start(&input, 14));
+    println!("Part 2: {}", message_start2(&input, 14));
 }
