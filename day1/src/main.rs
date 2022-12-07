09df911f564b3410afb4d85326bea6c4ebaca072
Marcus <6180175+MKindberg@users.noreply.github.com>
2022-12-07 20:22:00 +0100

    Improve day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,16 +2,10 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input").expect("Failed to read input file");
-    let mut elves : Vec<u32> = vec![];
-    let mut sum = 0;
-    for l in input.lines() {
-        if l.is_empty() {
-            elves.push(sum);
-            sum = 0;
-        } else {
-            sum += l.parse::<u32>().unwrap();
-        }
-    }
+    let mut elves: Vec<u32> = input
+        .split("\n\n")
+        .map(|x| x.lines().flat_map(|l| l.parse::<u32>()).sum())
+        .collect();
 
     elves.sort();
     println!("{}", elves.iter().rev().next().unwrap());
