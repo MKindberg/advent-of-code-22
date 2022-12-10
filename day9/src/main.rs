94b7cf2cd23bbdf840d28d6335b9de4f912c5139
Marcus <6180175+MKindberg@users.noreply.github.com>
2022-12-10 11:58:56 +0100

    Add function to animate day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::HashSet,
     fs,
-    ops::{Add, AddAssign, Sub},
+    ops::{Add, AddAssign, Sub}, os::unix::io
 };
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
@@ -58,20 +58,26 @@ fn mv_to(t: &mut Point, h: Point) {
     };
 }
 
-fn print(rope: &[Point]) {
-    let mut grid: Vec<Vec<i32>> = vec![vec![-1; 10]; 10];
+fn animate(rope: &[Point]) {
+    let esc = 27 as char;
+    let mut grid: Vec<Vec<i32>> = vec![vec![-1; 30]; 30];
     for (i, r) in rope.iter().enumerate().rev() {
-        grid[r.1 as usize][r.0 as usize] = i as i32;
+        grid[r.1.wrapping_add(10) as usize][r.0.wrapping_add(15) as usize] = i as i32;
     }
     for r in grid.iter().rev() {
         for c in r {
             if *c == -1 {
-                print!(".");
+                print!("  ");
             } else {
-                print!("{}", c);
+                // print!("{}", c);
+                print!("{}[47m  {}[49m", esc,esc);
             }
         }
         println!();
+    }
+    std::thread::sleep(core::time::Duration::from_millis(25));
+    for _ in grid {
+        print!("{}[A", esc);
     }
 }
 
@@ -79,18 +85,22 @@ fn unique_tail_pos(input: &str, rope_len: usize) -> usize {
     let mut positions: HashSet<Point> = HashSet::new();
     let mut rope: Vec<Point> = vec![Point(0, 0); rope_len];
     positions.insert(rope[0]);
+    // print(&rope);
+    // std::thread::sleep(core::time::Duration::from_millis(3000));
 
     for (dir, steps) in input.lines().flat_map(|x| x.split_once(' ')) {
         for _ in 0..steps.parse().unwrap() {
             rope[0] += mv_head(dir);
             let mut prev = rope[0];
-            for current in rope.iter_mut().skip(1) {
-                mv_to(current, prev);
-                prev = *current;
+            for c in 1..rope.len() {
+                mv_to(&mut rope[c], prev);
+                prev = rope[c];
+                // animate(&rope);
             }
             positions.insert(*rope.last().unwrap());
         }
     }
+    // std::thread::sleep(core::time::Duration::from_millis(3000));
     positions.len()
 }
 
@@ -98,4 +108,5 @@ fn main() {
     let input = fs::read_to_string("input").expect("Failed to read input file");
     println!("Part 1: {}", unique_tail_pos(&input, 2));
     println!("Part 2: {}", unique_tail_pos(&input, 10));
+
 }
