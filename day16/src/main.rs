e3ca83fa8a247a2f6c946a8c95b570cf8cd7cbb3
Marcus <6180175+MKindberg@users.noreply.github.com>
2022-12-17 14:44:59 +0100

    Optimize day 16

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -13,110 +13,43 @@ fn walk(
     activated: usize,
     current: usize,
     cache: &mut HashMap<(usize, usize, usize), usize>,
-) -> usize {
+) -> (usize, usize) {
+    let mut m_activated = activated;
     if minutes_left == 0 {
-        return 0;
+        return (0, activated);
     }
     if let Some(&x) = cache.get(&(current, activated, minutes_left)) {
-        return x;
+        return (x, activated);
     }
     let mut max_flow = 0;
     for &c in &rooms[current].connections {
-        max_flow = max_flow.max(walk(
-            rooms,
-            active_flow,
-            minutes_left - 1,
-            activated,
-            c,
-            cache,
-        ));
+        let (f, a) = walk(rooms, active_flow, minutes_left - 1, activated, c, cache);
+        if f > max_flow {
+            max_flow = f;
+            m_activated = a;
+        }
     }
     if rooms[current].flow != 0 && activated & (1 << current) == 0 {
-        max_flow = max_flow.max(walk(
+        let (f, a) = walk(
             rooms,
             active_flow + rooms[current].flow,
             minutes_left - 1,
             activated | (1 << current),
             current,
             cache,
-        ));
-    }
-    max_flow += active_flow;
-    cache.insert((current, activated, minutes_left), max_flow);
-    max_flow
-}
-
-fn walk2(
-    rooms: &Vec<Room>,
-    active_flow: usize,
-    minutes_left: usize,
-    activated: usize,
-    current: (usize, usize),
-    cache: &mut HashMap<((usize, usize), usize, usize), usize>,
-) -> usize {
-    if minutes_left == 0 {
-        return 0;
-    }
-    if let Some(&x) = cache.get(&(current, activated, minutes_left)) {
-        return x;
-    }
-    if let Some(&x) = cache.get(&((current.1, current.0), activated, minutes_left)) {
-        return x;
-    }
-    let mut max_flow = 0;
-    for &c0 in &rooms[current.0].connections {
-        for &c1 in &rooms[current.1].connections {
-            max_flow = max_flow.max(walk2(
-                rooms,
-                active_flow,
-                minutes_left - 1,
-                activated,
-                (c0, c1),
-                cache,
-            ));
-            if rooms[current.0].flow != 0 && activated & (1 << current.0) == 0 {
-                max_flow = max_flow.max(walk2(
-                    rooms,
-                    active_flow + rooms[current.0].flow,
-                    minutes_left - 1,
-                    activated | (1 << current.0),
-                    (current.0, c1),
-                    cache,
-                ));
-            }
-            if (rooms[current.1].flow != 0 && activated & (1 << current.1) == 0) {
-                max_flow = max_flow.max(walk2(
-                    rooms,
-                    active_flow + rooms[current.1].flow,
-                    minutes_left - 1,
-                    activated | (1 << current.1),
-                    (c0, current.1),
-                    cache,
-                ));
-            }
-            if current.0 != current.1
-                && (rooms[current.0].flow != 0 && activated & (1 << current.0) == 0)
-                && (rooms[current.1].flow != 0 && activated & (1 << current.1) == 0)
-            {
-                max_flow = max_flow.max(walk2(
-                    rooms,
-                    active_flow + rooms[current.0].flow + rooms[current.1].flow,
-                    minutes_left - 1,
-                    activated | (1 << current.0) | (1 << current.1),
-                    (current.0, current.1),
-                    cache,
-                ));
-            }
+        );
+        if f > max_flow {
+            max_flow = f;
+            m_activated = a;
         }
     }
     max_flow += active_flow;
     cache.insert((current, activated, minutes_left), max_flow);
-    max_flow
+    (max_flow, m_activated)
 }
 
 fn main() {
     let input = fs::read_to_string("input").expect("Failed to read input file");
-    // let input = fs::read_to_string("input2").expect("Failed to read input file");
 
     let mut str_to_int: HashMap<&str, usize> = HashMap::new();
     for (i, l) in input.lines().enumerate() {
@@ -143,23 +76,18 @@ fn main() {
 
     let mut cache: HashMap<(usize, usize, usize), usize> = HashMap::new();
     let timer = Instant::now();
-    let max_preassure = walk(&rooms, 0, 30, 0, str_to_int["AA"], &mut cache);
+    let (max_preassure, _) = walk(&rooms, 0, 30, 0, str_to_int["AA"], &mut cache);
     println!(
         "Part 1: {} in {}ms",
         max_preassure,
         timer.elapsed().as_millis()
     );
 
-    let mut cache: HashMap<((usize, usize), usize, usize), usize> = HashMap::new();
+    let mut cache: HashMap<(usize, usize, usize), usize> = HashMap::new();
     let timer = Instant::now();
-    let max_preassure = walk2(
-        &rooms,
-        0,
-        26,
-        0,
-        (str_to_int["AA"], str_to_int["AA"]),
-        &mut cache,
-    );
+    let (mut max_preassure, activated) = walk(&rooms, 0, 26, 0, str_to_int["AA"], &mut cache);
+    max_preassure += walk(&rooms, 0, 26, activated, str_to_int["AA"], &mut cache).0;
+
     println!(
         "Part 2: {} in {}ms",
         max_preassure,
